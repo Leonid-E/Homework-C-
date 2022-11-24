9720028ed28f46955fa75480bfcd557e2c7c4462
Leonid_Erchenko <leonidercenko@MacBook-Air-Leonid.local>
2022-11-24 21:12:32 +0300

    Вывод третьей цифры заданного числа

---
+++ Task_13/Program.cs
@@ -0,0 +1,18 @@
+﻿// Задача 13: Напишите программу, которая выводит третью цифру 
+// заданного числа или сообщает, что третьей цифры нет
+
+Console.WriteLine("Введите любое число: ");
+string a = Console.ReadLine();
+if (Convert.ToInt32(a) < 100) 
+{
+    Console.WriteLine ("Третьей цифры нет");
+} 
+else 
+{
+    char[] arr = new char [a.Length];
+    for (int i = 0; i < a.Length; i++) 
+    {
+        arr [i] = a[i];
+    }
+    Console.WriteLine ("Третья цифра : " + arr [2]);
+}
