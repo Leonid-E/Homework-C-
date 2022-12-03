59fdf6a89ca718fe624ced6d98e0a9ddbd27158d
Leonid_Erchenko <leonidercenko@MacBook-Air-Leonid.local>
2022-12-03 10:48:15 +0300

    Уточнил строки ввода и вывода

--- Task_25/Program.cs
+++ Task_25/Program.cs
@@ -1,10 +1,11 @@
-﻿// Задача 25: Напишите цикл, который принимает на вход два числа (A и B) и возводит число A в натуральную степень B.
+﻿// Задача 25: Напишите цикл, который принимает на вход два числа (A и B) и 
+// возводит число A в натуральную степень B.
 // 3, 5 -> 243 (3⁵)
 // 2, 4 -> 16
 
-Console.WriteLine("Введите первое число : ");
+Console.WriteLine("Введите первое число А : ");
 int a = Convert.ToInt16(Console.ReadLine());
-Console.WriteLine("Введите второе число : ");
+Console.WriteLine("Введите второе число В : ");
 int b = Convert.ToInt16(Console.ReadLine());
 double x = Math.Pow (a, b);
-Console.WriteLine(x);
+Console.WriteLine($"Число {a} в {b}-ой степени = {x}");
