f1af09f4b7981a5631c6c4cca35d1de861fd0aef
Leonid_Erchenko <leonidercenko@MacBook-Air-Leonid.local>
2022-12-04 00:09:13 +0300

    Массив из 8 элементов с выводом значений на экран

--- Task_29/Program.cs
+++ Task_29/Program.cs
@@ -5,7 +5,7 @@
 int[] array = new int [8];
 for (int i = 0; i < 8; i++)
     {
-        array[i] = new Random().Next(9);
+        array[i] = new Random().Next(100);
         Console.Write(array[i] + "; ");
     }
 
