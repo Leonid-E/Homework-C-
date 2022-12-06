1ebbabe4d3afaf7239c093f7d5d2970ce09ab381
Leonid_Erchenko <leonidercenko@MacBook-Air-Leonid.local>
2022-12-06 18:15:54 +0300

    Создание масива, проверка трёхзначных чисел на чётность

---
+++ Task_34/Program.cs
@@ -0,0 +1,25 @@
+﻿// Задача 34: Задайте массив заполненный случайными положительными трёхзначными числами. 
+// Напишите программу, которая покажет количество чётных чисел в массиве.
+// [345, 897, 568, 234] -> 2
+
+Console.Write("Создайте размер массива: ");
+int a = Convert.ToInt16(Console.ReadLine());
+int[] arrey = new int [a];
+int length = arrey.Length;
+int number_of_even_numbers = 0;
+int number_of_odd_numbers = 0;
+int i = 0;
+for (i = 0; i < length; i++)
+{
+    arrey[i] = new Random().Next(100, 1000); 
+    Console.Write(arrey[i] + " ");
+}  
+Console.WriteLine("Проверка чисел на чётность : ");
+for (i = 0; i < length; i++)
+{
+    if (arrey[i] % 2 == 0)
+    Console.WriteLine($"{arrey[i]} чётное ");
+    else
+    Console.WriteLine($"{arrey[i]} нечётное "); 
+}
+        
