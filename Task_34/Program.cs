3d5941fb0ed2927acd2d4bde9dacca71a8d9ea0c
Leonid_Erchenko <leonidercenko@MacBook-Air-Leonid.local>
2022-12-08 00:22:48 +0300

    Количество чётных чисел в массиве

--- Task_34/Program.cs
+++ Task_34/Program.cs
@@ -2,24 +2,26 @@
 // Напишите программу, которая покажет количество чётных чисел в массиве.
 // [345, 897, 568, 234] -> 2
 
-Console.Write("Создайте размер массива: ");
-int a = Convert.ToInt16(Console.ReadLine());
-int[] arrey = new int [a];
-int length = arrey.Length;
-int number_of_even_numbers = 0;
-int number_of_odd_numbers = 0;
-for (int i = 0; i < length; i++)
+Console.Write("Задайте размер массива: ");
+int size = Convert.ToInt16(Console.ReadLine());
+int[] arrey = new int[size];
+for (int i = 0; i < size; i++)
 {
     arrey[i] = new Random().Next(100, 1000); 
     Console.Write(arrey[i] + " ");
-}  
+}
 Console.WriteLine();
-Console.WriteLine("Проверка чисел на чётность: ");
-for (int i = 0; i < length; i++)
+Console.Write($"Чётные числа: ");
+int even_number = 0;
+int number_even_numbers = 0;
+for (int i = 0; i < size; i++) 
 {
     if (arrey[i] % 2 == 0)
-    Console.WriteLine($"{arrey[i]} чётное ");
-    else
-    Console.WriteLine($"{arrey[i]} нечётное "); 
+    {
+        even_number = arrey[i];
+        number_even_numbers++;
+        Console.Write(even_number + " ");
+    }
 }
-        
+Console.WriteLine();
+Console.WriteLine($"Количество чётных чисел: {number_even_numbers} ");
