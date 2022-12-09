4a45dccf5c04eb601ad67e1ac581fd49b4af6fa2
Leonid_Erchenko <leonidercenko@MacBook-Air-Leonid.local>
2022-12-09 17:10:16 +0300

    Два способа подсёта чисел, введённых пользователем, > 0

--- Task_41/Program.cs
+++ Task_41/Program.cs
@@ -12,14 +12,19 @@ for (int i = 0; i < arrey.Length; i++)
 }
 Console.WriteLine($"Кол-во элементов больше 0 : {num}"); 
 
+Console.WriteLine("Введите М-чисел через пробел : "); 
+string m = Console.ReadLine(); 
+int [] n = m.Split (' ').Select(Int32.Parse).ToArray();
+Console.Write("Массив: ");
+Console.WriteLine();
+foreach (int i in n)
+    Console.Write($"{i} ");
+int count = 0;
+for (int i = 0; i < n.Length; i++)
+{
+    if (n[i] > 0) 
+    count++;
+}
+Console.WriteLine();
+Console.WriteLine($"Кол-во элементов больше 0 : {count}");
 
-
-
-// int number_greater_than_0 = 1;
-// for (int i = 0; i < arrey.Length; i++)
-// {
-//     if (arrey[i] > 0)
-//     number_greater_than_0 = arrey[i]; 
-//     number_greater_than_0++;
-// }
-// Console.Write(number_greater_than_0 + " ");
