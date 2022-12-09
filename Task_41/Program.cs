0e333ddcc0833f72cc31d90c3c842594acd1187f
Leonid_Erchenko <leonidercenko@MacBook-Air-Leonid.local>
2022-12-09 11:26:56 +0300

    Сколько чисел больше 0

---
+++ Task_41/Program.cs
@@ -0,0 +1,25 @@
+﻿// Задача 41: Пользователь вводит с клавиатуры M чисел. 
+// Посчитайте, сколько чисел больше 0 ввёл пользователь.
+// 0, 7, 8, -2, -2 -> 2
+// 1, -7, 567, 89, 223-> 3
+
+Console.Write("Введите M-числа через пробелы: ");
+int[] arrey = Array.ConvertAll(Console.ReadLine().Split(), int.Parse);
+int num = 0; 
+for (int i = 0; i < arrey.Length; i++)
+{
+    if (arrey[i] > 0) num++;
+}
+Console.WriteLine($"Кол-во элементов больше 0 : {num}"); 
+
+
+
+
+// int number_greater_than_0 = 1;
+// for (int i = 0; i < arrey.Length; i++)
+// {
+//     if (arrey[i] > 0)
+//     number_greater_than_0 = arrey[i]; 
+//     number_greater_than_0++;
+// }
+// Console.Write(number_greater_than_0 + " ");
