bc3bba6133728f4a1b6c464cdf787f6ee8efc290
Leonid_Erchenko <leonidercenko@MacBook-Air-Leonid.local>
2022-12-21 00:22:50 +0300

    Сумма натуральных чисел от М до N

---
+++ Task_66/Program.cs
@@ -0,0 +1,23 @@
+﻿// Задача 66: Задайте значения M и N. 
+// Напишите программу, которая найдёт сумму натуральных элементов 
+// в промежутке от M до N.
+
+// M = 1; N = 15 -> 120
+// M = 4; N = 8. -> 30
+
+Console.Write("Введите число M : ");
+int m = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите число N : ");
+int n = Convert.ToInt32(Console.ReadLine());
+if (n < m)
+{
+    int temp = m;
+    m = n; 
+    n = temp;
+}
+int Sum(int num)
+{
+    if (num == m) return m;
+    return num + Sum(num - 1);
+}
+Console.WriteLine (Sum(n));
